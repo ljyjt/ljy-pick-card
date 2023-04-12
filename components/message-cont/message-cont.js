e30e3c9031652265d664a2ddc2a47ee398d1a107
luojiayi <luojiayi@dobest.com>
2023-04-12 18:24:37 +0800

    更新

--- components/message-cont/message-cont.js
+++ components/message-cont/message-cont.js
@@ -1,60 +1,39 @@
 // components/message-cont/message-cont.js
-import {logisticsList} from '../../data/data';
 Component({
   options: {
     styleIsolation: 'shared'
   },
   properties: {
+    item: {
+      type: Object,
+      value: {}
+    }
   },
   data: {
-    logisticsList,
-    steps: [
-      {
-        text: '已签收',
-        desc: '描述信息',
-      },
-      {
-        text: '待取件',
-        desc: '描述信息',
-      },
-      {
-        text: '运输中',
-        desc: '描述信息',
-      },
-      {
-        text: '待揽收',
-        desc: '描述信息',
-      },
-    ],
-    //箭头朝下的class: down下 ''上
-    isDown: '',
-    // 详情页是否展开:false不展开 true展开
-    detailDown: false,
-    currentIndex: [],
-    x: 22
+    active: 0,
+    flag: 0
   },
   methods: {
-    dropDown(e) {
-      // if(this.data.drop === 0) {
-      //   this.setData({drop: 1,isDown: 'down'})
-      // } else {
-      //   this.setData({drop: 0,isDown: ''})
-      // }
-
-      let index = e.currentTarget.dataset.index;
-      if(!this.data.currentIndex.includes(index)) {
-        this.data.currentIndex.push(index);
+    //下拉物流详情页
+    detialDropDown() {
+      let { flag } = this.data;
+      if(flag === 0) {
+        this.setData({flag: 1});
       } else {
-        this.setData({currentIndex: this.data.currentIndex.filter(item => item !== index)});
+        this.setData({flag: 0})
       }
-      console.log(this.data.currentIndex);
-
-      // let item = e.currentTarget.dataset.list;
-      // console.log(index,item);
-      
-      // if(!item.down) {
-      //   item.down = true;
-      // } 
+    },
+    //copy物流编号  
+    copyDelivery(e) {
+      // console.log(e.currentTarget.dataset.text);
+      wx.setClipboardData({
+        data: e.currentTarget.dataset.text
+      })
+    }
+  },
+  lifetimes: {
+    attached() {
+      // console.log("2222", this.data.item);
     }
   }
 })
